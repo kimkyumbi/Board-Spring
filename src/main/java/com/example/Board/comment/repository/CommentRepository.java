cd3c0d00ee381287cffc7b438cf45ef692116f60
kimkyumbi <kimgyeombi@naver.com>
2024-06-19 20:44:45 +0900

    @Repository 삭제

--- src/main/java/com/example/Board/comment/repository/CommentRepository.java
+++ src/main/java/com/example/Board/comment/repository/CommentRepository.java
@@ -2,8 +2,6 @@ package com.example.Board.comment.repository;
 
 import com.example.Board.comment.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
 }
