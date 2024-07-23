73602ccde140d2fa5b13caf496059f52004872d2
kimkyumbi <kimgyeombi@naver.com>
2024-07-23 11:36:41 +0900

    주석 달기

--- src/main/java/com/example/domain/comment/service/impl/CommentServiceImpl.java
+++ src/main/java/com/example/domain/comment/service/impl/CommentServiceImpl.java
@@ -8,6 +8,7 @@ import com.example.domain.post.entity.PostEntity;
 import com.example.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,14 +16,25 @@ public class CommentServiceImpl implements CommentService {
     private final CommentRepository commentRepository;
     private final PostRepository postRepository;
 
-    public void execute(CommentRequest commentPost, Long postId) {
+    @Override
+    @Transactional
+    /**
+     * comment를 작성하는 비즈니스 로직
+     *
+     * @param postId
+     * @param CommendRequest
+     */
+    public void execute(Long postId, CommentRequest commentPost) {
+        // PostEntity 객체를 생성해 postId로 게시물 저장
         PostEntity post = postRepository.getReferenceById(postId);
 
+        // CommentEntity 객체를 생성해 빌드 객체로 변환
         CommentEntity comment = CommentEntity.builder()
-                .post(post)
-                .text(commentPost.getText())
+                .post(post) // 저장한 PostEntity 객체
+                .text(commentPost.getText()) // CommentRequest에서 받은 Text를 저장
                 .build();
 
+        // DB에 저장
         commentRepository.save(comment);
     }
 }
