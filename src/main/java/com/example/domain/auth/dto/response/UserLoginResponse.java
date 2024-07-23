91af7dfd981f10802aadbd9565cea406065ba537
kimkyumbi <kimgyeombi@naver.com>
2024-07-23 11:37:46 +0900

    주석 달기

--- src/main/java/com/example/domain/auth/dto/response/UserLoginResponse.java
+++ src/main/java/com/example/domain/auth/dto/response/UserLoginResponse.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 유지가 로그인 시 응답을 위한 DTO
+ */
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserLoginResponse {
+    // 로그인했을 때 발급될 accessToken
     private String accessToken;
+    // 로그인했을 때 발급될 refreshToken
     private String refreshToken;
 }
