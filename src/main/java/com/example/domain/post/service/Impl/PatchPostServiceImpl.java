3595b755adb60ce3dee4d4df9c19b7e4a46535cc
kimkyumbi <kimgyeombi@naver.com>
2024-07-23 11:37:15 +0900

    주석 달기

--- src/main/java/com/example/domain/post/service/Impl/PatchPostServiceImpl.java
+++ src/main/java/com/example/domain/post/service/Impl/PatchPostServiceImpl.java
@@ -6,17 +6,28 @@ import com.example.domain.post.service.PatchPostService;
 import com.example.domain.post.dto.request.PatchPostRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PatchPostServiceImpl implements PatchPostService {
     private final PostRepository postRepository;
 
-    public void execute(PatchPostRequest patchPostRequest, Long id) {
-        PostEntity postEntity = postRepository.findById(id).orElseThrow(RuntimeException::new);
-
+    /**
+     * 게시글을 수정하는 비즈니스 로직
+     *
+     * @param patchPostRequest
+     * @param postId
+     */
+    @Override
+    @Transactional
+    public void execute(PatchPostRequest patchPostRequest, Long postId) {
+        // PostEntity 객체를 생성해 DB에서 postId로 게시글을 찾아 저장
+        // 오류가 생겼을 경우 RuntimeException 던지기
+        PostEntity postEntity = postRepository.findById(postId).orElseThrow(RuntimeException::new);
+        // postEntity에 저장된 게시글을 PostEntity의 updatePost 메서드로 수정
         postEntity.updatePost(patchPostRequest);
-
+        // DB에 수정된 내용을 저장
         postRepository.save(postEntity);
     }
 }
