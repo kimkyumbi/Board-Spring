13e72c21a7f21e90beafb9c2c8defd7b4089e874
kimkyumbi <kimgyeombi@naver.com>
2024-07-23 11:36:29 +0900

    주석 달기

--- src/main/java/com/example/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/domain/comment/controller/CommentController.java
@@ -12,12 +12,23 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
     private final CommentService commentService;
 
+    // 댓글 작성
     @PostMapping("/{post_id}")
+    /**
+     * 반환 타입 - ResponseEntity<CommentRequest>
+     * @PathVariable로 postId를 받음
+     * @RequestBody로 CommentRequest(postId, text) 값을 받음
+     *
+     * @param postId
+     * @param CommentRequest
+     */
     public ResponseEntity<CommentRequest> comment (
             @PathVariable(name = "post_id") Long postId,
             @RequestBody CommentRequest commentRequest
     ) {
-        commentService.execute(commentRequest, postId);
+        // commentService의 execute 메서드 호출
+        commentService.execute(postId, commentRequest);
+        // 상태 코드 200 ok와 함께 ResponseEntity 반환
         return ResponseEntity.ok().build();
     }
 }
