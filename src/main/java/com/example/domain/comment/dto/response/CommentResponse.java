56debbf437ba33add54ee8352df6dc924e6b3a73
kimkyumbi <kimgyeombi@naver.com>
2024-07-23 11:36:36 +0900

    주석 달기

--- src/main/java/com/example/domain/comment/dto/response/CommentResponse.java
+++ src/main/java/com/example/domain/comment/dto/response/CommentResponse.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 요청이 들어왔을 때 댓글을 보여주기 위한 DTO
+ */
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
