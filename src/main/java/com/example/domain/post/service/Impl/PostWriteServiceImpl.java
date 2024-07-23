e6c77cf8039da419880dafa87943358335a5291b
kimkyumbi <kimgyeombi@naver.com>
2024-07-23 11:37:29 +0900

    주석 달기

--- src/main/java/com/example/domain/post/service/Impl/PostWriteServiceImpl.java
+++ src/main/java/com/example/domain/post/service/Impl/PostWriteServiceImpl.java
@@ -6,18 +6,28 @@ import com.example.domain.post.service.PostWriteService;
 import com.example.domain.post.dto.request.PostRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PostWriteServiceImpl implements PostWriteService {
     private final PostRepository postRepository;
 
+    /**
+     * 게시글을 작성하는 비즈니스 로직
+     *
+     * @param postRequestDTO
+     */
+    @Override
+    @Transactional
     public void execute(PostRequestDTO postRequestDTO) {
+        // PostEntity 객체를 생성하고 빌더 객체로 변환
         PostEntity postEntity = PostEntity.builder()
-                .title(postRequestDTO.getTitle())
-                .content(postRequestDTO.getContent())
+                .title(postRequestDTO.getTitle()) // title에 postRequestDTO에서 받은 title 저장
+                .content(postRequestDTO.getContent()) // content에 postRequestDTO에서 받은 content 저장
                 .build();
 
+        // DB에 저장
         postRepository.save(postEntity);
     }
 }
