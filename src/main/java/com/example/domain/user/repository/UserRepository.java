b5d758244bbd76c50fb1cab0705d9d1cdbf8b3a6
kimkyumbi <kimgyeombi@naver.com>
2024-07-23 11:37:48 +0900

    주석 달기

--- src/main/java/com/example/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/domain/user/repository/UserRepository.java
@@ -4,8 +4,9 @@ import com.example.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     boolean existsByEmail(String email);
     Optional<UserEntity> findByEmail(String email);
 }
