efe35af2cfd58a0f416d52957fb9373297da276e
kimkyumbi <kimgyeombi@naver.com>
2024-07-23 11:36:52 +0900

    주석 달기

--- src/main/java/com/example/domain/post/service/GetPostByIdService.java
+++ src/main/java/com/example/domain/post/service/GetPostByIdService.java
@@ -3,5 +3,5 @@ package com.example.domain.post.service;
 import com.example.domain.post.dto.response.GetPostResponse;
 
 public interface GetPostByIdService {
-    GetPostResponse execute(Long id);
+    GetPostResponse execute(Long postId);
 }
